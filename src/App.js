948d6887902ba286246deb4b335b6c00063615d1
CHien <chihien12a5@gmail.com>
2024-12-03 11:03:32 +0700

    Main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Index from "./frontend/index";
+import { BrowserRouter as BrowserRouter,Routes,Route } from "react-router-dom";
+import FrontendRoute from "./routes/frontend";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} >
+        {FrontendRoute.map((route,index)=>{
+              const Page =route.component;
+          return (
+        
+            <Route key={index} path={route.path} element={<Page/>} />
+          )
+
+        })}
+        </Route>
+      </Routes>
+      </BrowserRouter>);
 }
 
 export default App;
